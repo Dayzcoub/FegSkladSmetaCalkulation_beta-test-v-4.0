de32b01357155cdf313ddcb4f7cca2bb081c964d
Dayzcoub <77221495+Dayzcoub@users.noreply.github.com>
2026-05-24 08:58:58 +0400

    chore: bump cache for equipment summary squeeze

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_VERSION = 'PACKIT_EQUIPMENT_TOOLBAR_FINAL_POLISH_2026_05_24';
-const CACHE_NAME = 'packit-equipment-toolbar-final-polish-2026-05-24';
-const RUNTIME_CACHE = 'packit-runtime-equipment-toolbar-final-polish-2026-05-24';
+const CACHE_VERSION = 'PACKIT_EQUIPMENT_SUMMARY_SQUEEZE_2026_05_24';
+const CACHE_NAME = 'packit-equipment-summary-squeeze-2026-05-24';
+const RUNTIME_CACHE = 'packit-runtime-equipment-summary-squeeze-2026-05-24';
 
 const CORE_ASSETS = [
   './',
